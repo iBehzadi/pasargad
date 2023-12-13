3f5d1020e07f805f7b97e133d7631a7e7e030cba
behzad <bsd098@gmail.com>
2023-12-13 23:34:48 +0330

    first commit

---
+++ src/js/main-script.js
@@ -0,0 +1,26 @@
+$(function () {
+    new WOW().init();
+    $('.nav-icon').on('click', function() {
+        $('.nav-menu').addClass('menu-collapse')
+        $('.nav-background').show()
+    });
+
+    $('.nav-close').on('click', function() {
+        $('.nav-menu').removeClass('menu-collapse')
+        $('.nav-background').hide()
+    })
+})
+
+
+$(window).on('load', function () {
+    // $('#status').fadeOut();
+    // $('#preloader').delay(350).fadeOut('slow');
+
+
+    $('.banner h1').addClass('animate__animated animate__fadeInDown animate__slow')
+    $('.banner div').addClass('animate__animated animate__fadeInUp animate__slow')
+
+    
+})
+
+
